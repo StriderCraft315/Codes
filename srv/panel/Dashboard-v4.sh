cee28287a4061f4ef4f21056d01bc70436008a1c
Ｎᴏʙɪᴛᴀ <nn8109239@gmail.com>
2026-01-12 16:52:58 +0530

    Update Dashboard-v4.sh

--- srv/panel/Dashboard-v4.sh
+++ srv/panel/Dashboard-v4.sh
@@ -104,7 +104,7 @@ make set-prod
   crontab -l 2>/dev/null | grep -v "/var/www/mythicaldash-v3/backend/storage/cron/runner.php"; \
   echo "* * * * * bash /var/www/mythicaldash-v3/backend/storage/cron/runner.bash >> /dev/null 2>&1"; \
   echo "* * * * * php /var/www/mythicaldash-v3/backend/storage/cron/runner.php >> /dev/null 2>&1"; } | crontab -
-
+clear
 php mythicaldash setup
 php mythicaldash migrate
 php mythicaldash pterodactyl configure
